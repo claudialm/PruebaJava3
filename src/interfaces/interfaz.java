c151c8c1503b4b661e0964a5684d6de206706e1e
claudialm <claudia_incondicional@hotmail.com>
2017-11-10 19:29:36 -0300

    Creacion clase con estructura Java

--- src/interfaces/interfaz.java
+++ src/interfaces/interfaz.java
@@ -4,11 +4,74 @@
  * and open the template in the editor.
  */
 package interfaces;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 /**
  *
  * @author LAB03
  */
-public class interfaz {
-    
+public class interfaz 
+{
+    private static interfaz inter;//Objeto tipo Connection
+    private static final String DRIVER = "com.mysql.jdbc.Driver";
+    private static final String USER = "root";
+    private static final String PASS = "";
+    private static final String URL = "jdbc:mysql://localhost:3306/PruebaJava3";
+
+    public interfaz()
+{
+    inter = null;
+ 
+     try
+ {
+        Class.forName(DRIVER);
+            inter = DriverManager.getConnection(URL, USER, PASS);
+            if (inter != null) 
+            {
+             System.out.println("Conexion Establecida");
+            }
+  }
+        catch(ClassNotFoundException | SQLException e)
+  {
+         System.out.println("Error en la Conexion");
+  }
 }
+    public Connection conectar()
+    {
+        return inter;  
+    }
+    
+    public void desconectar()
+    {
+      inter=null;
+        if (inter==null) 
+        {
+          System.out.println("Conexion Finalizada");
+
+        }
+        
+        }
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
