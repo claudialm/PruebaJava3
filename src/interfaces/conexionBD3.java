5bbd8ac97bf2b489e2f538a11ad8465430511c28
claudialm <claudia_incondicional@hotmail.com>
2017-11-10 19:57:36 -0300

    Esta clase recibe datos por ventana

--- src/interfaces/conexionBD3.java
+++ src/interfaces/conexionBD3.java
@@ -6,13 +6,67 @@
 package interfaces;
 import javax.swing.*;
 import java.awt.*;
+
+import bd.conexionBD3;
+import java.sql.Connection;
+import java.awt.event.*;
 /**
  *
  * @author LAB03
  */
-public class conexionBD3 
+public class conexionBD3 extends JFrame
 {
+    JPanel panel;
+    JLabel label;
+    JTextField text;
+    JButton boton;
+    JButton boton2;
+
+    conexionBD3 con;
+
+     public Interfaz()
+    {
+        panel = new JPanel();
+        label = new JLabel();
+        text = new JTextField(20);
+        boton = new JButton();
+        boton2 = new JButton();
+        
+        this.add(panel);
+        panel.add(label);
+        panel.add(text);
+        panel.add(boton);
+        panel.add(boton2);
+        
+        label.setText("Atributo");
+        boton.setText("Conectar");
+        boton2.setText("Desconectar");
+        panel.setBackground(Color.blue);
     
+       //PASO 16 - AGREGAMOS ESCUCHADORES AL BOTON
+        boton.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent evt)
+            {
+              con = new conexionBD3();
+              Connection reg = con.conectar();
+            }
+        });
+        
+        boton2.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent evt)
+            {
+               con.desconectar();
+            }
+        });
+
+
+
+    }
+
+
+
 
 
 
